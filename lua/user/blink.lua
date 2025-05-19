54ac423bc0f159b4c5aaf913b24f26ea79b5182a
Chris <christiano.rodrigues@shore.com>
2025-05-19 08:32:54 +0200

    add diagnostics and completion

--- lua/user/blink.lua
+++ lua/user/blink.lua
@@ -2,22 +2,21 @@
 
  blink.setup {
    fuzzy = { implementation = "lua" },
-   keymap = { preset = 'default' },
+   keymap = { preset = 'enter' },
+   -- (Default) Only show the documentation popup when manually triggered
+   completion = { documentation = { auto_show = false } },
+   snippets = { preset = 'luasnip' },
+   signature = { enabled = true },
    appearance = {
      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
      -- Adjusts spacing to ensure icons are aligned
-     nerd_font_variant = 'mono'
+     nerd_font_variant = 'normal'
    },
-
-   -- (Default) Only show the documentation popup when manually triggered
-   completion = { documentation = { auto_show = false } },
-
    sources = {
      default = { 'lsp', 'snippets', 'buffer', 'path' },
    },
  }
-
-local cfg = vim.lsp.config.elixirls
-vim.lsp.config('elixirls', {
-   capabilities = require('blink.cmp').get_lsp_capabilities(cfg.capabilities)
+-- local cfg = vim.lsp.config.elixirls
+-- vim.lsp.config('elixirls', {
+--    capabilities = require('blink.cmp').get_lsp_capabilities(cfg.capabilities)
 })
